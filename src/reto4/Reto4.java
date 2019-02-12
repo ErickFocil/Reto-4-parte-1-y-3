2d5f5d23ff8dc3c08ce0528b12a7f7ff20dd01e1
Erick Focil <edfocilg@hotmail.com>
2019-02-12 07:41:46 -0600

    Creacion del proyecto y anexo de la primera clase con parametros

---
+++ src/reto4/Reto4.java
@@ -0,0 +1,16 @@
+package reto4;
+
+/**
+ *
+ * @author edfoc
+ */
+public class Reto4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/reto4/SegundoPrograma.java
@@ -0,0 +1,19 @@
+package reto4;
+
+public class SegundoPrograma {
+    public String nombre;
+    public int edad;
+    public double altura;
+    
+    public void Saludar(){
+        
+    }
+    
+    public void Despedirse(){
+        
+    }
+    
+    public void Retirarse(){
+        
+    }
+}

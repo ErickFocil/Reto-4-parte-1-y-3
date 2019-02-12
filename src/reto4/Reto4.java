9eb29d0a58de7e38b02e27caeb8b20c8a985353e
Erick Focil <edfocilg@hotmail.com>
2019-02-12 08:06:14 -0600

    Mostrar el valor del boleto

--- src/reto4/Reto4.java
+++ src/reto4/Reto4.java
@@ -7,30 +7,30 @@ public class Reto4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char opcion;
-        double costo_boleto;
+        double costo_boleto = 0.00;
         
         do{
             System.out.println("Indique el tipo de boleto (A: Adulto, N: Niño, I: INAPAM, B: Adulto mayor sin credencial)");
             opcion = sc.next().charAt(0);
             switch(opcion){
                 case 'A':
-                    costo_boleto = 100;
+                    costo_boleto = 100.00;
                     break;
                 case 'N':
-                    costo_boleto = 70;
+                    costo_boleto = 70.00;
                     break;
                 case 'I':
-                    costo_boleto = 50;
+                    costo_boleto = 50.00;
                     break;
                 case 'B':
-                    costo_boleto = 70;
+                    costo_boleto = 70.00;
                     break;
                 default:
                     System.out.println("Opcion no disponible...");
                     break;
                 
             }
-            
+            System.out.println("El costo del boleto es: $" + costo_boleto);
             System.out.println("Desea otro boleto? (s/n)");
             opcion = sc.next().charAt(0);
         }while(opcion != 'n');
